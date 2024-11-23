54ddcdf24c69b86ed984a645af4caed3a468400a
4ndy10 <andysoto101205@gmail.com>
2024-11-23 13:59:08 -0800

    Concatenacion de strings con nombre

---
+++ concatenated1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    string word1 = get_string("Enter your name: ");
+    string word2 = "yupi!!!!!";
+    printf("%s, %s\n", word1, word2);
+}
